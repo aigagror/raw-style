701dca253d48cf306b0112b25bd80ae5405ed22e
eddie <aigagror@gmail.com>
2021-04-29 16:48:30 -0500

    added nobatchnorm test

--- tests/test_style.py
+++ tests/test_style.py
@@ -3,8 +3,10 @@ import tensorflow_addons as tfa
 from absl import flags
 from absl.testing import absltest
 
-from backbones import make_karras
+from backbones import make_karras, make_resnet152v2
 from style import make_discriminator
+from layers import NoBatchNorm
+from tensorflow import python as tf_python
 
 FLAGS = flags.FLAGS
 
@@ -21,6 +23,28 @@ class TestStyle(absltest.TestCase):
                         self.assertIsInstance(layer, tfa.layers.SpectralNormalization)
                     else:
                         self.assertNotIsInstance(layer, tfa.layers.SpectralNormalization)
+
+    def test_no_batch_norm(self):
+        backbone = make_resnet152v2(tf.keras.Input([224, 224, 3]))
+        layers = ['conv2_block1_out']
+        for spectral_norm in [False, True]:
+            discriminator = make_discriminator(backbone, layers, apply_spectral_norm=spectral_norm)
+            discriminator.summary()
+
+            num_batch_norms, num_no_batch_norms = 0, 0
+            for layer in discriminator.layers:
+                if isinstance(layers, tf.keras.layers.BatchNormalization) or \
+                   isinstance(layer, tf_python.keras.layers.normalization_v2.BatchNormalization):
+                    num_batch_norms += 1
+                if isinstance(layer, NoBatchNorm):
+                    num_no_batch_norms += 1
+
+            if spectral_norm:
+                self.assertEqual(num_batch_norms, 0)
+                self.assertGreater(num_no_batch_norms, 0)
+            else:
+                self.assertGreater(num_batch_norms, 0)
+                self.assertEqual(num_no_batch_norms, 0)
 
 
 if __name__ == '__main__':
